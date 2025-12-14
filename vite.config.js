377e5a38beed04cfceddb6585b9e9c62ae3755f9
srt-gt4065b <gt4065b@wsu.ac.kr>
2025-12-14 18:36:33 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/srt-member-db/', // GitHub 저장소 이름으로 변경
-})
